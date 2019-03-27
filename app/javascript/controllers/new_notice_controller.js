01608cc406dbee97807f3bb5d10d784d42ec2278
Rich Daley <rich@fishpercolator.co.uk>
2019-03-27 15:54:34 +0000

    Add some JS magick

---
+++ app/javascript/controllers/new_notice_controller.js
@@ -0,0 +1,43 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ 'location', 'contact', 'time', 'image', 'geo' ]
+
+  changed(event) {
+    switch (event.target.value) {
+      case 'ask':
+        this.locationTarget.style.display = 'none'
+        this.contactTarget.style.display = 'block'
+        this.timeTarget.style.display = 'none'
+        this.imageTarget.style.display = 'none'
+        break
+      case 'event':
+        this.locationTarget.style.display = 'block'
+        this.contactTarget.style.display = 'block'
+        this.timeTarget.style.display = 'block'
+        this.imageTarget.style.display = 'none'
+        break
+      case 'intro':
+        this.locationTarget.style.display = 'none'
+        this.contactTarget.style.display = 'none'
+        this.timeTarget.style.display = 'none'
+        this.imageTarget.style.display = 'block'
+        break
+      default:
+        this.locationTarget.style.display = 'none'
+        this.contactTarget.style.display = 'none'
+        this.timeTarget.style.display = 'none'
+        this.imageTarget.style.display = 'none'
+    }
+  }
+
+  initialize() {
+    // Just for show for now
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.geoTarget.innerHTML = `Your current location: ${pos.coords.latitude}, ${pos.coords.longitude}`
+      })
+    }
+    this.changed({target: {value: 'notice'}})
+  }
+}
