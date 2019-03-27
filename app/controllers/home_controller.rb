76b2e83c8ceefbf36707a22fe5770ae42991d5c7
Rich Daley <rich@fishpercolator.co.uk>
2019-03-27 15:09:23 +0000

    Cosmetic stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_after_action :verify_policy_scoped
   skip_after_action :verify_authorized
   def index
-    @notices = Notice.all
+    @notices = Notice.order(:created_at)
   end
 
   def manifest
